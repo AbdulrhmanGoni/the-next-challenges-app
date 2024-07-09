a969fdd05c869209767ee73eef41c8f83743a655
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-07-09 14:53:14 +0300

    feat: create `src/app/sign-up/page.tsx` file that represents the "sign up" page

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import SignUpForm from '@/components/login-signup/SignUpForm'
+
+export default function page() {
+    return (
+        <div className='flex items-center justify-center min-h-screen p-2'>
+            <SignUpForm />
+        </div>
+    )
+}
