9a61c1d9788960642bc7043d05a5159a02e0ecb3
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-07-14 17:19:56 +0300

    style: add styles classes to `body` and `main` elements
    - Add "flex" and "flex-col" classes to `body` element for making
    its children flow vertically.
    
    - Replace "min-h-screen" class from `main` element in `AppLayout`
    component with "flex-1" class for making the `main` element takes
    the height of its parent (the `body` element in `src/app/layout.tsx` file)
    instead of specifying a height for it using "min-h-screen" class.

--- src/app/AppLayout.tsx
+++ src/app/AppLayout.tsx
@@ -5,7 +5,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <Header />
-            <main className="flex min-h-screen flex-col items-center justify-between">
+            <main className="flex flex-1 flex-col items-center justify-between">
                 <div className="flex flex-1 w-full">
                     <Sidebar />
                     {children}
