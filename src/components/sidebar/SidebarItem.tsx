16e71b851246a2b3322432181b2daac3b56407ff
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-07-14 16:53:36 +0300

    feat: create `SidebarItem` component for the sidebar

---
+++ src/components/sidebar/SidebarItem.tsx
@@ -0,0 +1,34 @@
+"use client"
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import Tooltip from '../Tooltip'
+import { SidebarItemType } from './sidebarItemsData';
+
+export default function SidebarItem({ item }: { item: SidebarItemType }) {
+
+    const path = usePathname();
+    const itemBg = path === item.link ? "bg-primary" : "bg-transparent";
+    const itemBgOnHover = path === item.link ? "" : "hover:bg-primary-hover";
+    const linkStyles = `flex items-center gap-3 text-sm py-2 px-3 ${itemBg} ${itemBgOnHover} transition-colors`
+
+    return (
+        <li>
+            <Tooltip
+                triggerProps={{
+                    className: 'block sm:hidden ' + linkStyles,
+                }}
+                tooltipContent={item.title}
+                contentProps={{ side: "left" }}
+            >
+                <item.Icon size={19} />
+            </Tooltip>
+            <Link
+                href={item.link}
+                className={`hidden sm:flex text-sm gap-2 flex-1 ${linkStyles}`}
+            >
+                <item.Icon size={19} />
+                <p>{item.title}</p>
+            </Link>
+        </li>
+    )
+}
