b2587ddc87ea23718c6a5c780bed911e6a80063d
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-07-17 23:11:25 +0300

    feat: create `getHomePagePosts` server action
    Create `src/lib/getHomePagePosts.ts` file and create inside this file
    the GraphQL query definition of bringing posts for the user in the
    home page, and the server action function `getHomePagePosts`
    that sends the GraphQL query and receives the response.

---
+++ src/lib/getHomePagePosts.ts
@@ -0,0 +1,35 @@
+import graphglQueryAction from "@/lib/graphql/graphglQueryAction";
+import { gql } from "@apollo/client";
+
+const getPostsQuery = gql`
+  query UserFeeds {
+    userFeeds {
+      id
+      title
+      publishedAt
+      category
+      authorId
+      upvotes
+      downvotes
+      userVote
+      thumbnail {
+        src
+      }
+      tags
+      body
+      commentsCount
+      resources {
+        link
+        title
+        type
+      }
+    }
+  }
+`;
+
+export default async function getHomePagePosts() {
+  "use server";
+
+  const res = await graphglQueryAction<{ userFeeds: Post[] }>(getPostsQuery);
+  return res.data.userFeeds;
+}
