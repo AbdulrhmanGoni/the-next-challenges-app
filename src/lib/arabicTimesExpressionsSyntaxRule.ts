c30d9ecb5d6f74ffa53e927d5dfe23b14f005d9f
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-07-18 01:18:38 +0300

    feat: create `arabicTimesExpressionsSyntaxRule.ts` file
    Create `src/lib/arabicTimesExpressionsSyntaxRule.ts` file and create
    inside this file the different types of times expressions syntax and
    plural rules; create `getTimeSyntaxRule` function that takes the time
    type and the value and returns the right time expresstion to express
    the given time.

---
+++ src/lib/arabicTimesExpressionsSyntaxRule.ts
@@ -0,0 +1,63 @@
+import convertToArabic from "./convertToArabic";
+
+type Time = "second" | "minute" | "hour" | "day" | "week" | "month" | "year";
+type SyntaxRule = "one" | "two" | "few" | "many";
+
+export function getTimeSyntaxRule(time: Time, value: number) {
+  const pluralRule = new Intl.PluralRules("ar-EG").select(value);
+  const timeExpression =
+    timeExpressionsSyntaxRules[time][
+      new Intl.PluralRules("ar-EG").select(value) as SyntaxRule
+    ];
+
+  if (pluralRule === "one" || pluralRule === "two") {
+    return timeExpression;
+  }
+
+  return `${convertToArabic(value)} ${timeExpression}`;
+}
+
+export const timeExpressionsSyntaxRules = {
+  second: {
+    one: "ثانية واحدة",
+    two: "ثانيتين",
+    few: "ثانية",
+    many: "ثواني",
+  },
+  minute: {
+    one: "دقيقة واحدة",
+    two: "دقيقتين",
+    few: "دقائق",
+    many: "دقيقة",
+  },
+  hour: {
+    one: "ساعة واحدة",
+    two: "ساعتين",
+    few: "ساعات",
+    many: "ساعة",
+  },
+  day: {
+    one: "يوم واحد",
+    two: "يومين",
+    few: "أيام",
+    many: "يوم",
+  },
+  week: {
+    one: "أسبوع واحد",
+    two: "اسبوعين",
+    few: "أسابيع",
+    many: "أسبوع",
+  },
+  month: {
+    one: "شهر واحد",
+    two: "شهرين",
+    few: "شهور",
+    many: "شهر",
+  },
+  year: {
+    one: "سنة واحدة",
+    two: "سنتين",
+    few: "سنوات",
+    many: "سنة",
+  },
+};
