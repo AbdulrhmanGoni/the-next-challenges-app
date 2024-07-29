0b5af5d0772e1f4b71e50df281ddf3df7899d969
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-07-29 13:51:17 +0300

    feat: change the response type of `userFeeds` query
    Change the response type of `userFeeds` query from `Post[]` to
    `UserFeedsResponse` in `getHomePagePosts` server action.
    
    Include the default pagination model to user feeds query
    via `paginationOptions` property of variables parameter
    of `graphglQueryAction` function.

--- src/lib/getHomePagePosts.ts
+++ src/lib/getHomePagePosts.ts
@@ -1,9 +1,14 @@
 import graphglQueryAction from "@/graphql/graphglQueryAction";
-import { getPostsQuery } from "../graphql/UserFeedsQuery";
+import {
+  defaultUserFeedsPaginationModel,
+  getPostsQuery,
+} from "../graphql/UserFeedsQuery";
 
 export default async function getHomePagePosts() {
   "use server";
-
-  const res = await graphglQueryAction<{ userFeeds: Post[] }>(getPostsQuery);
+  const res = await graphglQueryAction<{ userFeeds: UserFeedsResponse }>(
+    getPostsQuery,
+    { paginationOptions: defaultUserFeedsPaginationModel }
+  );
   return res.data.userFeeds;
 }
