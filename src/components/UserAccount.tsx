6d17c006f8dcc365ff6b1018265f5558a56e44e8
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-07-31 17:27:57 +0300

    refactor: replace a deleted component in `UserAccount.tsx`
    Replace the deleted `IconButton` component and `Link` inside
    `UserAccount` component with `Button` component and
    `useRouter` hook for navigation.

--- src/components/UserAccount.tsx
+++ src/components/UserAccount.tsx
@@ -1,25 +1,28 @@
 "use client"
 import useUserData from "@/hooks/useUserData";
 import { LogInIcon } from "lucide-react";
-import IconButton from "./IconButton";
-import Link from "next/link";
 import { Skeleton } from "./shadcn-ui/skeleton";
 import UserAccountMenu from "./UserAccountMenu";
+import { useRouter } from "next/navigation";
+import { Button } from "./shadcn-ui/button";
 
 export default function UserAccount() {
 
     const { userData, requestDone } = useUserData();
+    const { push } = useRouter();
 
     return (
         userData ?
             <UserAccountMenu />
             : requestDone ? (
-                <Link href="/log-in">
-                    <IconButton className="bg-muted hover:bg-primary-hover p-1.5 rounded-md cursor-pointer">
-                        تسجيل الدخول
-                        <LogInIcon size={22} strokeWidth={1.5} />
-                    </IconButton>
-                </Link>
+                <Button
+                    className="bg-muted hover:bg-primary-hover text-nowrap gap-2"
+                    size="default"
+                    onClick={() => push("/log-in")}
+                >
+                    {"تسجيل الدخول"}
+                    <LogInIcon size={22} strokeWidth={1.5} />
+                </Button>
             ) : <Skeleton className="w-10 h-10 rounded-full" />
     )
 }
