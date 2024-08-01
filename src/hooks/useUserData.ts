d7879c2b8ca1d5c341650d4e8d790d105231f048
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-08-01 17:06:51 +0300

    refactor: create `contexts` dir & move `UserDataProvider.tsx` into it

--- src/hooks/useUserData.ts
+++ src/hooks/useUserData.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { UserDataContext } from "@/app/UserDataProvider";
+import { UserDataContext } from "@/contexts/UserDataProvider";
 
 export default function useUserData() {
   return useContext(UserDataContext);
