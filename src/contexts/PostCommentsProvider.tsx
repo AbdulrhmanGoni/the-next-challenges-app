2711b021573afa6c93050b7c71127e93a39159a6
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-08-11 15:05:50 +0300

    feat: create `PostCommentsProvider` component
    Create `PostCommentsProvider` component for providing
    the state of post comments for all components under post
    card component by calling the hook that fetches post comments
    (`usePostCommentsPagination`) inside it and share the results
    using react context feature.

---
+++ src/contexts/PostCommentsProvider.tsx
@@ -0,0 +1,63 @@
+import React, { createContext, PropsWithChildren } from 'react'
+import usePostCommentsPagination from '@/hooks/usePostCommentsPagination';
+
+type PostCommentsContextType = {
+    postId: Post["id"];
+    comments: PostComment[];
+    areThereMoreComments: boolean,
+    isLoading: boolean,
+    error?: string,
+    addComment: (comment: PostComment) => void;
+    removeComment: (select: (post: PostComment) => boolean) => void;
+    loadMoreComments: () => void;
+    paginationModel: PaginationModel
+}
+
+export const PostCommentsContext = createContext<PostCommentsContextType>({
+    postId: "",
+    comments: [],
+    isLoading: false,
+    areThereMoreComments: true,
+    error: "",
+    addComment() { },
+    removeComment() { },
+    loadMoreComments() { },
+    paginationModel: { page: 1, pageSize: 1 }
+})
+
+type PostCommentsProviderProps = PropsWithChildren<{
+    postId: Post["id"];
+    postCommentsCount: number
+}>
+
+export default function PostCommentsProvider({ children, postId, postCommentsCount }: PostCommentsProviderProps) {
+
+    const {
+        comments,
+        areThereMoreComments,
+        isLoading,
+        error,
+        addComment,
+        removeComment,
+        loadMoreComments,
+        paginationModel
+    } = usePostCommentsPagination({ postId, postCommentsCount });
+
+    return (
+        <PostCommentsContext.Provider
+            value={{
+                postId,
+                comments,
+                areThereMoreComments,
+                isLoading,
+                error,
+                addComment,
+                removeComment,
+                loadMoreComments,
+                paginationModel
+            }}
+        >
+            {children}
+        </PostCommentsContext.Provider>
+    )
+}
