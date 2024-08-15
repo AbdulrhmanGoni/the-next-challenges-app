13fc8ee6ec43ca6d061674b71b35d151fad301a4
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-08-15 14:05:45 +0300

    feat: display author's avatar in `PostAuthor` component
    Pass the url of author's avatar to `src` prop of `Avatar` component
    in `PostAuthor` component.

--- src/components/post-card/PostAuthor.tsx
+++ src/components/post-card/PostAuthor.tsx
@@ -3,7 +3,10 @@ import Avatar from '../Avatar'
 export default function PostAuthor({ author }: { author: Post["author"] }) {
     return (
         <div className="flex items-center gap-2 py-2">
-            <Avatar fallback='AC' />
+            <Avatar
+                src={author.avatar?.src}
+                fallback={author.firstName[0] + author.lastName[0]}
+            />
             <div className="me-5">
                 <p className="text-md">{author.firstName} {author.lastName}</p>
                 <p className="text-ms text-muted-foreground">{author.headline}</p>
