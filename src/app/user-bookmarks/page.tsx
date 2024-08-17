c957ccb5db39a66fa61c44fbb622b10cf42a2efb
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-08-17 17:25:00 +0300

    feat: create user bookmarks page
    Create the page component that represents user bookmarks
    page in `src/app/user-bookmarks/page.tsx`, which is consist
    of the app layout component (`AppLayout`) and as child of it,
    the container of the page `UserBookmarksContainerPage`
    that contains all the content of the page.

---
+++ src/app/user-bookmarks/page.tsx
@@ -0,0 +1,10 @@
+import AppLayout from "../AppLayout";
+import UserBookmarksContainerPage from "@/components/user-bookmarks-page/UserBookmarksContainerPage";
+
+export default function UserBookmarksPage() {
+  return (
+    <AppLayout>
+      <UserBookmarksContainerPage />
+    </AppLayout>
+  )
+}
